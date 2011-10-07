c04de10187d443c03b0e9d00d5e18acffd5716a6
Tyron Madlener <tyronx@gmail.com>
2011-10-07 16:12:14 +0200

    wrong jquery path

--- setup.php
+++ setup.php
@@ -2,7 +2,7 @@
 <html>
 <head>
 <title>Anego CMS Setup</title>
-<script type="text/javascript" src="lib/jquery.js"></script>
+<script type="text/javascript" src="lib/jquery-1.6.2.min.js"></script>
 <style type="text/css">
 img {
 	border:0;
