2167f9bd8e56c81d6349f9c070a165dbd0b2f976
Tyron Madlener <tyronx@gmail.com>
2012-01-14 20:52:33 +0100

    removed dependency on php5.2 function json_encode()

--- core.php
+++ core.php
@@ -14,10 +14,72 @@ require "inc/html.php";
 
 /**** Basic Checks ****/
 
-if(!function_exists('json_encode'))
-	echo 'Warning: json_encode() not Available. Image Upload might not work properly (old PHP Version?)';
+if (!function_exists('json_encode')) {
+	//echo 'Warning: json_encode() not Available. Image Upload might not work properly (old PHP Version?)';
+	function json_encode( $data ) {           
+		if( is_array($data) || is_object($data) ) { 
+			$islist = is_array($data) && ( empty($data) || array_keys($data) === range(0,count($data)-1) ); 
+			
+			if( $islist ) { 
+				$json = '[' . implode(',', array_map('json_encode', $data) ) . ']'; 
+			} else { 
+				$items = Array(); 
+				foreach( $data as $key => $value ) { 
+					$items[] = json_encode("$key") . ':' . json_encode($value); 
+				} 
+				$json = '{' . implode(',', $items) . '}'; 
+			} 
+		} elseif( is_string($data) ) { 
+			# Escape non-printable or Non-ASCII characters. 
+			# I also put the \\ character first, as suggested in comments on the 'addclashes' page. 
+			$string = '"' . addcslashes($data, "\\\"\n\r\t/" . chr(8) . chr(12)) . '"'; 
+			$json    = ''; 
+			$len    = strlen($string); 
+			# Convert UTF-8 to Hexadecimal Codepoints. 
+			for( $i = 0; $i < $len; $i++ ) { 
+				
+				$char = $string[$i]; 
+				$c1 = ord($char); 
+				
+				# Single byte; 
+				if( $c1 <128 ) { 
+					$json .= ($c1 > 31) ? $char : sprintf("\\u%04x", $c1); 
+					continue; 
+				} 
+				
+				# Double byte 
+				$c2 = ord($string[++$i]); 
+				if ( ($c1 & 32) === 0 ) { 
+					$json .= sprintf("\\u%04x", ($c1 - 192) * 64 + $c2 - 128); 
+					continue; 
+				} 
+				
+				# Triple 
+				$c3 = ord($string[++$i]); 
+				if( ($c1 & 16) === 0 ) { 
+					$json .= sprintf("\\u%04x", (($c1 - 224) <<12) + (($c2 - 128) << 6) + ($c3 - 128)); 
+					continue; 
+				} 
+					
+				# Quadruple 
+				$c4 = ord($string[++$i]); 
+				if( ($c1 & 8 ) === 0 ) { 
+					$u = (($c1 & 15) << 2) + (($c2>>4) & 3) - 1; 
+				
+					$w1 = (54<<10) + ($u<<6) + (($c2 & 15) << 2) + (($c3>>4) & 3); 
+					$w2 = (55<<10) + (($c3 & 15)<<6) + ($c4-128); 
+					$json .= sprintf("\\u%04x\\u%04x", $w1, $w2); 
+				} 
+			} 
+		} else { 
+			# int, floats, bools, null 
+			$json = strtolower(var_export( $data, true )); 
+		} 
+		return $json; 
+	} 
+}
 	
-if(!file_exists('styles/'.STYLE.'/templates/index.tpl'))
+if (!file_exists('styles/'.STYLE.'/templates/index.tpl'))
 	exit("File 'styles/".STYLE."/templates/index.tpl' not found. Missing or mistyped style name?");
 
 if (isset($_REQUEST['_SESSION'])) die("Get lost Muppet!");
@@ -437,7 +499,7 @@ function pageLoadJs($p) {
 }
 
 // Returns required module-js files per page when editing
-function pageEditJs($p) {
+function pageEditJs() {
 	if(file_exists('var/installed_modules'))
 		$modules = unserialize(file_get_contents('var/installed_modules'));
 	else $modules = Array();
