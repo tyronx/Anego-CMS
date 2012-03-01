220c2986a197008d51f3949cb53d5605ff8dd3ee
Tyron Madlener <tyronx@gmail.com>
2012-03-01 23:10:43 +0100

    Corrected linklist urls

--- modules/richtext/linkList.js.php
+++ modules/richtext/linkList.js.php
@@ -13,13 +13,18 @@ if (! LOGINOK) {
 
 $res2=mysql_query("SELECT idx,name FROM ".PAGES);
 $i=0;
+
 while($row2=mysql_fetch_array($res2)) {
 	if($i>0) echo ',';
 	$name=str_replace('"','\\"',$row2['name']);
-	if($cfg['fancyURLs']) echo '["'.$name.'", "pg'.$row2['idx'].'"]';
-	else echo '["'.$name.'", "index.php?p='.$row2['idx'].'"]';
+	if($cfg['fancyURLs']) {
+		echo '["' . $name . '", ' . $cfg['path'] . '"pages/' . $row2['idx'] . '"]';
+	} else {
+		echo '["'.$name.'", "index.php?p=' . $row2['idx'] . '"]';
+	}
 	$i++;
 }
+
 echo ');';
 
 ?>
