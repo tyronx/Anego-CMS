8bb5feda697b2bebc199026412f12e54a3c30a71
Tyron Madlener <tyronx@gmail.com>
2012-03-11 00:47:28 +0100

    removed unused aloha editor

--- modules/alohatext/alohatext.js
+++
@@ -1,7 +0,0 @@
-alohatext = ContentElement.extend({
-	onStartEdit: function(newlyCreated) {
-		if (! newlyCreated)
-			alert('Aloha settings not implemented yet. To edit the text with aloha, please leave the edit mode');
-		return false;
-	}
-});
--- setup.php
+++ setup.php
@@ -423,5 +423,3 @@ function createTables() {
 	}
 	return true;
 }
-
-?>
