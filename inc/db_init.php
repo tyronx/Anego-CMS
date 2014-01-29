4e28c570ac509b9603d5f3a26bae3da68e784dda
Tyron Madlener <tyronx@gmail.com>
2014-01-29 21:17:24 +0100

    Set a bunch of charset configs to UTF-8 on DB Init

--- inc/db_init.php
+++ inc/db_init.php
@@ -11,3 +11,5 @@ if(!@mysql_select_db(SQLDB)) {
 	$sql_link=0;
 	BailErr(__('Our Database is not reachable. Please try again later!'),mysql_error(),true);
 }
+
+mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'");
