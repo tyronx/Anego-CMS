66dfb115a9db4148dd0ad6b99ce57cdf75cdd8d6
Tyron Madlener <tyronx@gmail.com>
2015-12-30 10:10:58 +0100

    Added alphabetic sorting of the image list

--- modules/richtext/imageList.js.php
+++ modules/richtext/imageList.js.php
@@ -14,14 +14,23 @@ if (! LOGINOK) {
 $imgpath = 'files/content';
 // Get all page which are not just for structuring (nolink)
 $dir = opendir($imgpath);
-$i=0;
 
-while ($file = readdir($dir)) {
+
+$files = array();
+
+while ($file = readdir($dir)) {	
 	if (preg_match("/(?<!_r)\.(jpg|png|gif)$/i", $file)) {
-		if ($i > 0) echo ',';
-		echo '["' . $file . '", "' . $cfg['path'] . $imgpath . '/' . $file . '"]';
-		$i++;
+		$files[] = $file;
 	}
+}
+
+natsort($files);
+
+$i=0;
+foreach ($files as $file) {
+	if ($i > 0) echo ',';
+	echo '["' . $file . '", "' . $cfg['path'] . $imgpath . '/' . $file . '"]';
+	$i++;
 }
 
 echo ');';
